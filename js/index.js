4adb7fb25d4b67faf2d822bee6815f41396d0f37
Jasper Jenkins <jasper.vs.jenkins@gmail.com>
2018-04-25 16:43:07 -0700

    made changes

--- js/index.js
+++ js/index.js
@@ -1,10 +1,14 @@
+d3.csv("data/data.csv", function(data) {
+  console.log(data[0]);
+});
+
 var rectDemo = d3.select("#d3_0").
   append("svg").
-  attr("width", 400).
-  attr("height", 300);
+  attr("width", 500).
+  attr("height", 500);
 
 rectDemo.append("rect").
-  attr("x", 50).
-  attr("y", 50).
+  attr("x", 0).
+  attr("y", 0).
   attr("height", 100).
-  attr("width", 200);
+  attr("width", 100);
