28bfe6cc5834edca8a84720a09a0a5bbf89fcbc2
Jasper Jenkins <jasper.vs.jenkins@gmail.com>
2018-04-24 22:06:41 -0700

    changes

--- js/index.js
+++ js/index.js
@@ -1,13 +1,10 @@
-
 var rectDemo = d3.select("#d3_0").
   append("svg").
   attr("width", 400).
   attr("height", 300);
 
 rectDemo.append("rect").
-  attr("x", 100).
-  attr("y", 100).
+  attr("x", 50).
+  attr("y", 50).
   attr("height", 100).
   attr("width", 200);
-
-alert('HALP')
