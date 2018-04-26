b0ee7f631e5cd31a5c4cae888a7da063b37323c7
Jasper Jenkins <jasper.vs.jenkins@gmail.com>
2018-04-26 15:18:29 -0700

    adjust axis

--- js/index.js
+++ js/index.js
@@ -20,17 +20,20 @@ d3.csv("data/data.csv", function(error, data) {
 
     let x = d3.scaleLinear()
         .range([0, width])
-        .domain([0, d3.max(data, function(d) { return d.x;})]);
+        .domain([-1, d3.max(data, function(d) { return d.x;})]);
 
     let y = d3.scaleLinear()
         .range([height, 0])
-        .domain([-2, d3.max(data, function(d) { return d.y;}) + 2]);
+        .domain([-1.5, d3.max(data, function(d) { return d.y;}) + 2]);
 
-    graph.append("g")
-        .attr("transform", "translate(0," + height + ")")
+        graph.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(0," + y(0) + ")")
         .call(d3.axisBottom(x));
-        // Add the Y Axis
+    
     graph.append("g")
+        .attr("class", "axis axis--y")
+        .attr("transform", "translate(" + x(0) + ",0)")
         .call(d3.axisLeft(y));
 
     let circles = graph.selectAll('circle')
