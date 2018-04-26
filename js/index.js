8b4f57f3d2878ace42c45064dc1a0e20f728d226
Jasper Jenkins <jasper.vs.jenkins@gmail.com>
2018-04-26 08:29:02 -0700

    made changes

--- js/index.js
+++ js/index.js
@@ -1,14 +1,5 @@
-d3.csv("data/data.csv", function(data) {
-  console.log(data[0]);
-});
+const size = 600
 
-var rectDemo = d3.select("#d3_0").
-  append("svg").
-  attr("width", 500).
-  attr("height", 500);
-
-rectDemo.append("rect").
-  attr("x", 0).
-  attr("y", 0).
-  attr("height", 100).
-  attr("width", 100);
+graph = d3.select('#graph').append('svg')
+  .attr('width', size)
+  .attr('height', size)
